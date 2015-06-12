19b5e62292856b7b9045c1b0fdea761304cc825f
andycarmona <andres.carmona@uni-app.se>
2015-06-12 17:58:58 +0200

    External directive to handle svg
    Different methods in directive.
    Preparing to pass data to svg directive to render nodes,lines etc

--- ShowMeNow.Web/app/app.js
+++ ShowMeNow.Web/app/app.js
@@ -1,16 +1,5 @@
 ﻿
-var app = angular.module('AngularAuthApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar', 'kendo.directives']).directive('snapTest', function () {
-    return function (scope, element, attrs) {
-        var s = Snap(element[0]);
-        var bigCircle = s.circle(150, 150, 100);
-        bigCircle.attr({
-            fill: "#bada55",
-            stroke: "#000",
-            strokeWidth: 5
-        });
-        var smallCircle = s.circle(100, 150, 70);
-    }
-});;
+var app = angular.module('AngularAuthApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar', 'kendo.directives', 'SvgDirectives']);
 
 app.config(function ($routeProvider) {
 
---
+++ ShowMeNow.Web/app/directives/SvgDirectives.js
@@ -0,0 +1,36 @@
+﻿angular.module('SvgDirectives', [])
+.directive('snapTest', function () {
+    return function(scope, element, attrs) {
+       
+     
+
+
+        var initializeSvg = function() {
+            scope.s = Snap(element[0]);
+          
+        }
+
+        var AddCircle=function() {
+            var bigCircle = scope.s.circle(150, 150, 100);
+            bigCircle.attr({
+                fill: "#bada55",
+                stroke: "#000",
+                strokeWidth: 5
+            });
+        }
+
+      var addTextBox = function() {
+            var textValid = scope.s.text(0, 20, "Testeo");
+            textValid.attr({
+                'id': '1_textvalid',
+                'font-size': '25px',
+                'fill': 'green',
+                'stroke': 'green',
+                'stroke-width': 0.2
+            });
+      }
+      initializeSvg();
+      AddCircle();
+      addTextBox();
+    }
+});

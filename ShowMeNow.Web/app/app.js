b8f5659944acd4e6b04e91b7b13453445a1ef537
andycarmona <andres.carmona@uni-app.se>
2015-06-12 17:34:57 +0200

    Added snap svg directive and svg example

--- ShowMeNow.Web/app/app.js
+++ ShowMeNow.Web/app/app.js
@@ -1,5 +1,16 @@
 ﻿
-var app = angular.module('AngularAuthApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar','kendo.directives']);
+var app = angular.module('AngularAuthApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar', 'kendo.directives']).directive('snapTest', function () {
+    return function (scope, element, attrs) {
+        var s = Snap(element[0]);
+        var bigCircle = s.circle(150, 150, 100);
+        bigCircle.attr({
+            fill: "#bada55",
+            stroke: "#000",
+            strokeWidth: 5
+        });
+        var smallCircle = s.circle(100, 150, 70);
+    }
+});;
 
 app.config(function ($routeProvider) {
 

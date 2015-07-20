44ba4ffe20e5f59d8a89851dfb6b7336381dfe80
andycarmona <andres.carmona@uni-app.se>
2015-07-20 16:54:53 +0200

    Add markers to map from data in api.eniro

--- ShowMeNow.Web/app/controllers/placesController.js
+++ ShowMeNow.Web/app/controllers/placesController.js
@@ -2,6 +2,7 @@
 app.controller('placesController', ['$scope','placesService',  function ($scope,placesService) {
 
     $scope.places = [];
+
     //placesService.InitializeDataBase();
     $scope.iframeHeight = window.innerHeight;
     $scope.iframeWidth = window.innerWidth;
@@ -12,14 +13,37 @@ app.controller('placesController', ['$scope','placesService',  function ($scope,
        
     }
 
-   
+    $scope.image = {
+        url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+        size: [20, 32],
+        origin: [0, 0],
+        anchor: [0, 32]
+    };
+    $scope.shape = {
+        coords: [1, 1, 1, 20, 18, 20, 18, 1],
+        type: 'poly'
+    };
+    $scope.beaches = [
+      ['Bondi Beach', 57.709421, 11.964304, 4],
+      ['Coogee Beach', 57.709203, 11.963671, 5],
+      ['Cronulla Beach', -34.028249, 151.157507, 3],
+      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+      ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ];
 
     $scope.monthPickerConfig = {
         start: "year",
         depth: "year",
         format: "MMMM yyyy"
     };
+    var markers = [];
+    for (var i = 0; i < 10 ; i++) {
+        markers[i] = new google.maps.Marker({
+            title: "Hi marker " + i
+        });
+    }
 
+   
     //$scope.source = new kendo.data.DataSource({
     //    transport: {
     //        read: {
@@ -29,6 +53,22 @@ app.controller('placesController', ['$scope','placesService',  function ($scope,
     //    },
     //    pageSize: 2
     //});
+ 
+        //$scope.hotel = results.CompanyInfo.companyName;
+    placesService.GetExternalList().then(function (d) {
+      
+        $scope.adverts = d.data.adverts;
+
+        for (i = 0; i < 8; i++) {
+            var lat = parseFloat(d.data.adverts[i].location.coordinates[0].latitude);
+            var lng = parseFloat(d.data.adverts[i].location.coordinates[0].longitude);
+            if ((lat != undefined) || (lng != undefined)) {
+                var latlng = new google.maps.LatLng(lat, lng);
+                markers[i].setPosition(latlng);
+                markers[i].setMap($scope.map);
+            }
+        }
+    });
 
     placesService.GetAllPlaces().then(function (results) {
         $scope.source = new kendo.data.DataSource({
@@ -51,6 +91,8 @@ app.controller('placesController', ['$scope','placesService',  function ($scope,
     }, function (error) {
         //alert(error.data.message);
     });
+
+
 
     //$scope.products = new kendo.data.DataSource({
     //    data: [
--- ShowMeNow.Web/app/services/placesService.js
+++ ShowMeNow.Web/app/services/placesService.js
@@ -25,10 +25,20 @@ app.factory('placesService', ['$http', 'ngAuthSettings', function ($http, ngAuth
         return $http.get(serviceBase + 'api/Places/GetAllPeople');
     };
 
+    var _getExternalList = function() {
+        var promise = $http.jsonp('http://api.eniro.com/cs/search/basic?country=se&geo_area=gothenburg&version=1.1.3&search_word=hoteller&latitude=57.709421&longitud=11.964304&max_distance=100&key=330905261700999336&profile=andyw&to_list=5&callback=JSON_CALLBACK').then(function (response) {
+
+            return response;
+        });
+        return promise;
+    }
+
     placesServiceFactory.InitializeDataBase = _initializeDB;
     placesServiceFactory.AddInitialPeople = _addInitialPeople;
     placesServiceFactory.GetAllPlaces = _getAllPlaces;
     placesServiceFactory.GetAllPeople = _getAllPeople;
+    placesServiceFactory.GetExternalList = _getExternalList;
+
     return placesServiceFactory;
 
 }]);
